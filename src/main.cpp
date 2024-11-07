799f02f816d6b8248cf8bec758c858ea966bf306
fredericbroussolle <109629580+fredericbroussolle@users.noreply.github.com>
2024-11-07 21:00:15 +0400

    Update main.cpp
    Suppression des variables

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,18 @@
 #include <DHT.h>
 
 // Configuration WiFi
-const char* ssid = "ZEOP-db49b3";          // Remplacez par le nom de votre réseau WiFi
-const char* password = "ZTEGC8333ef6"; // Remplacez par votre mot de passe WiFi
+const char* ssid = "xxxxxxxxx";          // Remplacez par le nom de votre réseau WiFi
+const char* password = "xxxxxxxxx"; // Remplacez par votre mot de passe WiFi
 
 // Configuration du broker MQTT
-const char* mqtt_server = "192.168.1.15"; // Remplacez par l'adresse IP de votre broker MQTT
+const char* mqtt_server = "xxxxxxxxxxx"; // Remplacez par l'adresse IP de votre broker MQTT
 const int mqtt_port = 1883;
+const char* topicTemp = "xxxxxxxxx";          // Remplacez par le nom de votre réseau WiFi
+const char* topicHum = "xxxxxxxxx"; // Remplacez par votre mot de passe WiFi
 
 // Configuration du capteur DHT11
-#define DHTPIN 2        // Broche où le Data du DHT22 est connecté
-#define DHTTYPE DHT22    // Type du capteur (DHT22)
+#define DHTPIN xxxxxxx       // Broche où le Data du DHT22 est connecté
+#define DHTTYPE xxxxxxx    // Type du capteur (DHT22/DHT11)
 DHT dht(DHTPIN, DHTTYPE);
 
 // Création des objets WiFi et MQTT
@@ -80,8 +82,8 @@ void loop() {
   String humidity_str = String(humidity);
 
   // Publication des données sur les topics MQTT
-  client.publish("e06/temperature", temperature_str.c_str());
-  client.publish("e06/humidite", humidity_str.c_str());
+  client.publish(topicTemp, temperature_str.c_str());
+  client.publish(topicHum, humidity_str.c_str());
 
   // Affichage des valeurs dans le moniteur série
   Serial.print("Température : ");
